760a9151ffabbca4b5c80dec2cdf3c10246c3b93
mfakhrull <mfakrul383@gmail.com>
2023-06-07 08:44:21 +0800

    home_screen update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // This package is used for formatting the dates
 import 'package:murni_bus_ticket/services/ticket_service.dart';
 import 'package:murni_bus_ticket/models/ticket.dart';
-import 'package:murni_bus_ticket/models/user.dart'; // Import the User model
+import 'package:murni_bus_ticket/models/user.dart';
+import 'package:murni_bus_ticket/screens/view_update_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  final User user; // Add the user object as a parameter
+  final User user;
 
-  HomeScreen({required this.user}); // Add the user object to the constructor
+  HomeScreen({required this.user});
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -41,44 +43,81 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Bookings'),
+        title:
+            Text('My Bookings', style: Theme.of(context).textTheme.headline6),
       ),
       body: FutureBuilder<List<Ticket>>(
         future: _ticketFuture,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            // If the future has data, display the bookings
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 Ticket ticket = snapshot.data![index];
-                return ListTile(
-                  title: Text('Booking #${ticket.bookId ?? "default"}'),
-                  subtitle: Text(
-                      '${ticket.departDate ?? DateTime.now()} ${ticket.time ?? "default"} - ${ticket.destStation ?? "default"}'),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () => _deleteTicket(ticket.bookId),
+                return Card(
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(
+                      'Booking #${ticket.bookId ?? "default"}',
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          '${ticket.departStation ?? "default"} to ${ticket.destStation ?? "default"}',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        Text(
+                          '${DateFormat.yMMMd().format(ticket.departDate ?? DateTime.now())} ${ticket.time ?? "default"}',
+                          style: Theme.of(context).textTheme.caption,
+                        ),
+                      ],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.edit,
+                              color: Theme.of(context).primaryColor),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UpdateTicketScreen(
+                                  ticket: ticket,
+                                  user: widget.user,
+                                ),
+                              ),
+                            ).then((_) {
+                              setState(() {
+                                _ticketFuture = _ticketService
+                                    .getBookings(widget.user.userId ?? 0);
+                              });
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () => _deleteTicket(ticket.bookId),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
             );
           } else if (snapshot.hasError) {
-            // If the future has an error, display the error
             return Text('Error: ${snapshot.error}');
           } else {
-            // If the future is still loading, display a loading indicator
-            return CircularProgressIndicator();
+            return Center(child: CircularProgressIndicator());
           }
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Navigate to the BookingFormScreen when the button is pressed
-          Navigator.pushNamed(context, '/booking',
-                  arguments: widget.user) // Pass the user object as an argument
+          Navigator.pushNamed(context, '/booking', arguments: widget.user)
               .then((_) {
-            // Refresh the list of tickets after the BookingFormScreen is popped
             setState(() {
               _ticketFuture =
                   _ticketService.getBookings(widget.user.userId ?? 0);
